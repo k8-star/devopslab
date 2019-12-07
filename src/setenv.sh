d8f03d04cc22bd2305c95e8b009e214b2bcb1583
k8-star <58278818+k8-star@users.noreply.github.com>
2019-12-07 14:57:33 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=ruitais_cn@163.com
 export MYCLUSTER=tektonknativebeijing59
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing59/kube-config-tok04-tektonknativebeijing59.yml
 
-export NAMESPACE=
+export NAMESPACE=tektonknativebeijing59
 export EMAIL=sdcors@163.com
-export INGRESS=
+export INGRESS=tektonknativebeijing59-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
